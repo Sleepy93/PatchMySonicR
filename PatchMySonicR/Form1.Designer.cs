65cbd1c29e230ef30cd09d1c290d6302bc806392
Álmos Borlai <lmos Borlai>
2016-12-24 23:23:48 +0100

    Upload Code

---
+++ PatchMySonicR/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace PatchMySonicR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cb_drive = new System.Windows.Forms.ComboBox();
+            this.lb_drivetext = new System.Windows.Forms.Label();
+            this.cb_res = new System.Windows.Forms.ComboBox();
+            this.btn_exepatch = new System.Windows.Forms.Button();
+            this.btn_respatch = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cb_drive
+            // 
+            this.cb_drive.FormattingEnabled = true;
+            this.cb_drive.Location = new System.Drawing.Point(118, 5);
+            this.cb_drive.MaxLength = 1;
+            this.cb_drive.Name = "cb_drive";
+            this.cb_drive.Size = new System.Drawing.Size(155, 21);
+            this.cb_drive.TabIndex = 0;
+            // 
+            // lb_drivetext
+            // 
+            this.lb_drivetext.AutoSize = true;
+            this.lb_drivetext.Location = new System.Drawing.Point(7, 8);
+            this.lb_drivetext.Name = "lb_drivetext";
+            this.lb_drivetext.Size = new System.Drawing.Size(105, 13);
+            this.lb_drivetext.TabIndex = 1;
+            this.lb_drivetext.Text = "Select Drive\'s Letter:";
+            // 
+            // cb_res
+            // 
+            this.cb_res.FormattingEnabled = true;
+            this.cb_res.Location = new System.Drawing.Point(117, 32);
+            this.cb_res.Name = "cb_res";
+            this.cb_res.Size = new System.Drawing.Size(155, 21);
+            this.cb_res.TabIndex = 2;
+            // 
+            // btn_exepatch
+            // 
+            this.btn_exepatch.Location = new System.Drawing.Point(197, 60);
+            this.btn_exepatch.Name = "btn_exepatch";
+            this.btn_exepatch.Size = new System.Drawing.Size(75, 23);
+            this.btn_exepatch.TabIndex = 3;
+            this.btn_exepatch.Text = "Patch It!";
+            this.btn_exepatch.UseVisualStyleBackColor = true;
+            this.btn_exepatch.Click += new System.EventHandler(this.btn_exepatch_Click);
+            // 
+            // btn_respatch
+            // 
+            this.btn_respatch.Location = new System.Drawing.Point(117, 59);
+            this.btn_respatch.Name = "btn_respatch";
+            this.btn_respatch.Size = new System.Drawing.Size(75, 23);
+            this.btn_respatch.TabIndex = 3;
+            this.btn_respatch.Text = "Patch Res!";
+            this.btn_respatch.UseVisualStyleBackColor = true;
+            this.btn_respatch.Click += new System.EventHandler(this.btn_respatch_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Custom Resolution:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 90);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_respatch);
+            this.Controls.Add(this.btn_exepatch);
+            this.Controls.Add(this.cb_res);
+            this.Controls.Add(this.lb_drivetext);
+            this.Controls.Add(this.cb_drive);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Patch my SonicR";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cb_drive;
+        private System.Windows.Forms.Label lb_drivetext;
+        private System.Windows.Forms.ComboBox cb_res;
+        private System.Windows.Forms.Button btn_exepatch;
+        private System.Windows.Forms.Button btn_respatch;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ PatchMySonicR/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PatchMySonicR
+{
+    public partial class Form1 : Form
+    {
+        const string ExeName = "sonicr.exe";
+        const string FixedName = "sonicr_patched.exe";
+        const string ConfigName = "sonicr.inf";
+
+        const uint UnlockPtr = 0x00070584;
+        const uint UnlockLen = 0x23;
+
+        const uint DrivePtr = 0x0011D2FC;
+        const uint DriveLen = 0xC4;
+
+        const uint ResPointer = 0x1C;
+        const uint ResLen = 0x0C;
+
+        static byte[] Original =
+        {
+            0x2E, 0xFF, 0x15, 0x8C, 0x05, 0x95, 0x00, 0x29, 0xF8, 0x83, 0xC3, 0x04,
+            0x99, 0x31, 0xD0, 0x29, 0xD0, 0x01, 0xC6, 0x89, 0x44, 0x2B, 0xC0, 0x83,
+            0xFB, 0x28, 0x74, 0x1B, 0x2E, 0xFF, 0x15, 0x8C, 0x05, 0x95, 0x00
+        };
+        static byte[] Cracked =
+        {
+            0xB8, 0x31, 0x00, 0x00, 0x00, 0x90, 0x90, 0x29, 0xF8, 0x83, 0xC3, 0x04,
+            0x99, 0x31, 0xD0, 0x29, 0xD0, 0x01, 0xC6, 0x89, 0x44, 0x2B, 0xC0, 0x83,
+            0xFB, 0x28, 0x74, 0x1B, 0xB8, 0x01, 0x00, 0x00, 0x00, 0x90, 0x90
+        };
+
+        byte[] DriveInfo =
+        {
+            0x64, 0x3A, 0x5C, 0x00, 0x63, 0x3A, 0x5C, 0x00, 0x63, 0x3A, 0x5C, 0x00,
+            0x64, 0x3A, 0x5C, 0x00, 0x64, 0x3A, 0x5C, 0x00, 0x65, 0x3A, 0x5C, 0x00,
+            0x65, 0x3A, 0x5C, 0x00, 0x66, 0x3A, 0x5C, 0x00, 0x66, 0x3A, 0x5C, 0x00,
+            0x67, 0x3A, 0x5C, 0x00, 0x67, 0x3A, 0x5C, 0x00, 0x68, 0x3A, 0x5C, 0x00,
+            0x68, 0x3A, 0x5C, 0x00, 0x69, 0x3A, 0x5C, 0x00, 0x69, 0x3A, 0x5C, 0x00,
+            0x6A, 0x3A, 0x5C, 0x00, 0x6A, 0x3A, 0x5C, 0x00, 0x6B, 0x3A, 0x5C, 0x00,
+            0x6B, 0x3A, 0x5C, 0x00, 0x6C, 0x3A, 0x5C, 0x00, 0x6C, 0x3A, 0x5C, 0x00,
+            0x6D, 0x3A, 0x5C, 0x00, 0x6D, 0x3A, 0x5C, 0x00, 0x6E, 0x3A, 0x5C, 0x00,
+            0x6E, 0x3A, 0x5C, 0x00, 0x6F, 0x3A, 0x5C, 0x00, 0x6F, 0x3A, 0x5C, 0x00,
+            0x70, 0x3A, 0x5C, 0x00, 0x70, 0x3A, 0x5C, 0x00, 0x71, 0x3A, 0x5C, 0x00,
+            0x71, 0x3A, 0x5C, 0x00, 0x72, 0x3A, 0x5C, 0x00, 0x72, 0x3A, 0x5C, 0x00,
+            0x73, 0x3A, 0x5C, 0x00, 0x73, 0x3A, 0x5C, 0x00, 0x74, 0x3A, 0x5C, 0x00,
+            0x74, 0x3A, 0x5C, 0x00, 0x75, 0x3A, 0x5C, 0x00, 0x75, 0x3A, 0x5C, 0x00,
+            0x76, 0x3A, 0x5C, 0x00, 0x76, 0x3A, 0x5C, 0x00, 0x77, 0x3A, 0x5C, 0x00,
+            0x77, 0x3A, 0x5C, 0x00, 0x78, 0x3A, 0x5C, 0x00, 0x78, 0x3A, 0x5C, 0x00,
+            0x79, 0x3A, 0x5C, 0x00, 0x79, 0x3A, 0x5C, 0x00, 0x7A, 0x3A, 0x5C, 0x00,
+            0x7A, 0x3A, 0x5C, 0x00
+        };
+
+        static char[] res_delim = { '*', 'x', 'X' };
+
+        #region CommentBlock
+        // Details of Crack
+        #region Crack
+        // UnlockThe Game
+        // 00070584 - 000705A6 
+        // original:    2EFF158C05950029F883C3049931D029D001C689442BC083FB28741B2EFF158C059500
+        // fixed:       B831000000909029F883C3049931D029D001C689442BC083FB28741BB8010000009090
+
+        //Fix Drive's Letter:
+        //0011D2FC - 0011D3BF
+        // original:    d:\.c:\.c:\.d:\.d:\.e:\.e:\.f:\.f:\.g:\.g:\.h:\.h:\.i:\.i:\.j:\.j:\.k:\.k:\.l:\.l:\.m:\.m:\.n:\.n:\.o:\.o:\.p:\.p:\.q:\.q:\.r:\.r:\.s:\.s:\.t:\.t:\.u:\.u:\.v:\.v:\.w:\.w:\.x:\.x:\.y:\.y:\.z:\.z:\
+        // fixed:       h:\.h:\.h:\.h:\.h:\.h:\.h:\.h:\.h:\.h:\.h:\.h:\.h:\.h:\.h:\.h:\.h:\.h:\.h:\.h:\.h:\.h:\.h:\.h:\.h:\.h:\.h:\.h:\.h:\.h:\.h:\.h:\.h:\.h:\.h:\.h:\.h:\.h:\.h:\.h:\.h:\.h:\.h:\.h:\.h:\.h:\.h:\.h:\.h:\
+        #endregion
+
+        //Details of Resolution Fix
+        #region ResolutionFix
+        /*
+        all data: len:0C
+        00001C - 000028
+        Width:  00001C - 00001D
+        Height: 00001E - 00001F
+        Width:  000020 - 000021
+        Height: 000022 - 000023
+        Width:  000024 - 000025
+        Height: 000026 - 000027
+        */
+
+        #endregion
+        #endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_exepatch_Click(object sender, EventArgs e)
+        {
+            byte[] tmp = File.ReadAllBytes(ExeName);
+            for (int i = 0; i < UnlockLen; i++)
+                if (tmp[UnlockPtr + i] != Original[i])
+                {
+                    //Very nem egyezik
+                    if (MessageBox.Show("Maybe patched, path anyway?", Text,
+                        MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+                        break;
+                    else
+                        goto next;
+                }
+            for (int i = 0; i < UnlockLen; i++)
+                tmp[UnlockPtr + i] = Cracked[i];
+            next:
+            //Check
+            for (int i = 0; i < DriveLen; i++)
+                if (tmp[DrivePtr + i] != DriveInfo[i])
+                {
+                    //Very nem egyezik
+                    if (MessageBox.Show("Maybe patched, path anyway?", Text,
+                        MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+                        break;
+                    else
+                        goto end;
+                }
+            if (cb_drive.Text.Length == 0) goto end;
+            byte ltr = Encoding.ASCII.GetBytes(cb_drive.Text)[0];
+            for (int i = 0; i < DriveLen; i += 4)
+                tmp[DrivePtr + i] = ltr;
+            end:
+            File.WriteAllBytes(FixedName, tmp);
+            MessageBox.Show("Completed.", Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btn_respatch_Click(object sender, EventArgs e)
+        {
+            byte[] tmp = File.ReadAllBytes(ConfigName);
+            byte[] rep;
+            if (cb_res.Text.Length == 0)
+                rep = BitConverter.GetBytes((ushort)Screen.PrimaryScreen.Bounds.Width << 16 | (ushort)Screen.PrimaryScreen.Bounds.Height);
+            else
+            {
+                string[] spl = cb_res.Text.Split(res_delim);
+                ushort width, height;
+                if (ushort.TryParse(spl[0], out width) && ushort.TryParse(spl[1], out height))
+                    rep = BitConverter.GetBytes(width << 16 | height);
+                else
+                {
+                    if (MessageBox.Show("Not valid resolution. Use native resolution?", Text,
+                        MessageBoxButtons.YesNo, MessageBoxIcon.Error) == DialogResult.Yes)
+                        rep = BitConverter.GetBytes((ushort)Screen.PrimaryScreen.Bounds.Width << 16 | (ushort)Screen.PrimaryScreen.Bounds.Height);
+                    else
+                        return;
+                }
+            }
+            for (int i = 0; i < 12; i++)
+                tmp[ResPointer + i] = rep[i % 4];
+            File.WriteAllBytes(ConfigName, tmp);
+        }
+    }
+}
